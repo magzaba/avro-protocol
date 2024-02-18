9f8c9ea0e3741ac5483b7e68f02d958361028448
Magdalena <Magdalena_Zaba@epam.com>
2024-02-18 21:34:44 +0100

    Project setup and avro schema generation

---
+++ src/test/java/mzaba/avroprotocol/AvroProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package mzaba.avroprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroProtocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
